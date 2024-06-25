55817ad77ff2b58fd751919c37634bca922cf007
wosh1ngton <woshington.rod@gmail.com>
2024-06-25 09:50:54 -0300

    funcionalidade de login

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = "auth_data";
+
 @Injectable({
     providedIn: 'root'
 })
@@ -18,13 +20,21 @@ export class AuthStore {
     constructor(private http : HttpClient) {
         this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+        const user = localStorage.getItem(AUTH_DATA);
+
+        if(user) {
+           this.subject.next(JSON.parse(user));
+        }
     }
 
     login(email:string, password:string) : Observable<User> {
 
         return this.http.post<User>("/api/login", {email, password})
             .pipe(
-                tap(user => this.subject.next(user)),
+                tap(user => {
+                    this.subject.next(user);
+                    localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+                }),
                 shareReplay()
             );
 
@@ -32,5 +42,6 @@ export class AuthStore {
 
     logout() {
         this.subject.next(null);
+        localStorage.removeItem(AUTH_DATA);
     }
 }
